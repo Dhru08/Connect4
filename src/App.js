659d2bb447e8ec822b9dd3bc7be41ef09fc3519f
Dhruv <dhru2602@gmail.com>
2023-09-30 12:05:59 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const numRows = 6;
+    const numCols = 7;
+    const cellSize = 50;
+
+    const [selectedColumn, setSelectedColumn] = useState(null);
+
+    const handleColumnClick = (columnIndex) => {
+        setSelectedColumn(columnIndex);
+        console.log(`Selected column: ${columnIndex}`);
+    };
+
+    const renderTableRows = () => {
+        const rows = [];
+
+        for (let i = 0; i < numRows; i++) {
+            const row = [];
+            for (let j = 0; j < numCols; j++) {
+                row.push(
+                    <td
+                        key={`${i}-${j}`}
+                        style={{ width: cellSize, height: cellSize, cursor: 'pointer' }}
+                        onClick={() => handleColumnClick(j)}
+                    >
+                        
+                    </td>
+                );
+            }
+            rows.push(
+                <tr key={i} style={{ height: cellSize }}>
+                    {row}
+                </tr>
+            );
+        }
+
+        return rows;
+    };
+
+    return (
+        <>
+            <div className="container" style={{ width: "500px", height: "500px" }}>
+                <table className="table table-bordered mt-5">
+                    <tbody>{renderTableRows()}</tbody>
+                </table>
+            </div>
+            <p>Selected column: {selectedColumn !== null ? selectedColumn : 'None'}</p>
+        </>
+    );
 }
 
 export default App;
