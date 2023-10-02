961da48d0bd2e6ad160dad622f0c1fabc148af5a
Dhruv <dhru2602@gmail.com>
2023-10-02 12:53:58 +0530

    Added Simple AI into the AI game mode and refactored the code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import Board from './components/Board';
 import Navbar from './components/Navbar';
+import PvPBoard from './components/PvPBoard';
+import PvAIBoard from './components/PvAIBoard';
+import {
+    BrowserRouter,
+    Route,
+    Routes
+} from "react-router-dom";
 
 function App() {
     return (
-        <>
-            <Navbar title="Connect4"/>
-            <Board />
-        </>
+        <BrowserRouter>
+            <Navbar title="Connect4" />
+            <Routes>
+                <Route key="home" exact path="/" element={<PvPBoard />} />
+                <Route key="PvP" exact path="/PvP" element={<PvPBoard />} />
+                <Route key="PvAI" exact path="/PvAI" element={<PvAIBoard />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/PvAIBoard.js
@@ -0,0 +1,168 @@
+import React, { useState, useEffect } from 'react';
+import './Board.css';
+import Tile from './Tile';
+import PlayerInfo from './PlayerInfo';
+
+const Board = () => {
+    const playerRed = "R";
+    const playerYellow = "Y";
+    const rows = 6;
+    const cols = 7;
+    const maxMoves = rows * cols;
+
+    const [currPlayer, setCurrPlayer] = useState(playerRed);
+    const [gameOver, setGameOver] = useState(false);
+    const [board, setBoard] = useState([]);
+    const [currColumns, setCurrColumns] = useState(Array.from({ length: cols }, () => rows - 1));
+    const [moves, setMoves] = useState(0);
+    const [winner, setWinner] = useState(null);
+
+    useEffect(() => {
+        initializeBoard();
+    }, []);
+
+    function initializeBoard() {
+        const newBoard = [];
+        for (let r = 0; r < rows; r++) {
+            let row = [];
+            for (let c = 0; c < cols; c++) {
+                row.push(' ');
+            }
+            newBoard.push(row);
+        }
+        setBoard(newBoard);
+    }
+
+    function checkWin(row, col) {
+        const directions = [
+            [1, 0],
+            [0, 1],
+            [1, 1],
+            [1, -1]
+        ];
+
+        for (const [dx, dy] of directions) {
+            let count = 1;
+            count += countConsecutive(row, col, dx, dy);
+            count += countConsecutive(row, col, -dx, -dy);
+
+            if (count >= 4) {
+                setGameOver(true);
+                setWinner(currPlayer);
+                return;
+            }
+        }
+    }
+
+    function countConsecutive(row, col, dx, dy) {
+        let count = 0;
+        let r = row + dx;
+        let c = col + dy;
+
+        while (isValid(r, c) && board[r][c] === board[row][col]) {
+            count++;
+            r += dx;
+            c += dy;
+        }
+
+        return count;
+    }
+
+    function isValid(row, col) {
+        return row >= 0 && row < rows && col >= 0 && col < cols;
+    }
+
+    function handleTileClick(c) {
+        if (gameOver || currPlayer === playerYellow) {
+            return;
+        }
+
+        let r = currColumns[c];
+        if (r < 0) {
+            // show some kind of alert message
+            return;
+        }
+
+        const newColumns = [...currColumns];
+        newColumns[c] -= 1;
+
+        if (board[r][c] === ' ') {
+            board[r][c] = currPlayer;
+            checkWin(r, c);
+            setCurrColumns(newColumns);
+            setCurrPlayer(currPlayer === playerRed ? playerYellow : playerRed);
+            setMoves(moves + 1);
+        }
+    }
+
+    function makeComputerMove(c) {
+        if (gameOver) {
+            return;
+        }
+
+        let r = currColumns[c];
+        if (r < 0) {
+            // show some kind of alert message
+            return;
+        }
+
+        const newColumns = [...currColumns];
+        newColumns[c] -= 1;
+
+        if (board[r][c] === ' ') {
+            board[r][c] = currPlayer;
+            checkWin(r, c);
+            setCurrColumns(newColumns);
+            setCurrPlayer(currPlayer === playerRed ? playerYellow : playerRed);
+            setMoves(moves + 1);
+        }
+    }
+
+    function AImove() {// Implement more sophisticated algorithm.
+        let c = Math.floor(Math.random() * cols);
+        while (currColumns[c] < 0) {
+            c = Math.floor(Math.random() * cols);
+        }
+        return c;
+    }
+
+    /* eslint-disable */
+    useEffect(() => {
+        // Check if it's the AI's turn (playerYellow) and make its move
+        if (currPlayer === playerYellow) {
+            setTimeout(() => {
+                makeComputerMove(AImove());
+            }, 1000);
+        }
+    }, [currPlayer]); // Include makeComputerMove as a dependency 
+    /* eslint-enable */
+
+    function isDraw() {
+        return moves === maxMoves;
+    }
+
+    function renderBoard() {
+        return board.map((row, rowIndex) => (
+            row.map((tile, colIndex) => (
+                <Tile
+                    key={cols * rowIndex + colIndex}
+                    tile={tile}
+                    onClick={() => handleTileClick(colIndex)}
+                    playerRed={playerRed}
+                    playerYellow={playerYellow}
+                />
+            ))
+        ));
+    }
+
+    return (
+        <div className="d-flex flex-column justify-content-center align-items-center">
+            <PlayerInfo currentPlayer={currPlayer} isDraw={isDraw()} winner={winner} /> {/* Include PlayerInfo component */}
+            <div className="m-4" id="board">
+                {renderBoard()}
+            </div>
+        </div>
+    );
+}
+
+export default React.memo(Board);
